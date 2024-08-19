9e792bb00ba4adb348c3ae8eab83c02ae0b6e22e
Mks-DevOps <168957860+Mks-DevOps@users.noreply.github.com>
2024-08-19 12:52:35 +0530

    Update ec2_instance.tf

--- terraform/modules/ec2_instance.tf
+++ terraform/modules/ec2_instance.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "example" {
-  
+module "ec2-Instance" {
+  source = "./module"
+  ami_value = "ami-04a81a99f5ec58529"
+  instance_type_value = "t2.micro"
+
 }
