850d7b4fba33d3cc8d67628a4aef98890a004cd2
Mks-DevOps <168957860+Mks-DevOps@users.noreply.github.com>
2024-08-19 12:53:27 +0530

    Update ec2_instance.tf

--- terraform/modules/ec2_instance.tf
+++ terraform/modules/ec2_instance.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
+#calling module_example dir, here we do not have to define variable
 module "ec2-Instance" {
   source = "./module"
   ami_value = "ami-04a81a99f5ec58529"
